4c6467f60eee3e286e71578d5d66b9b04f7397a3
NepthalieBryntAsinero <asinero.nepthaliebrynt@gmail.com>
2026-02-21 11:07:22 +0800

    commit

--- my_website/settings.py
+++ my_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-wsv$aau0w_pm_l6)q4x-eae*)!2uej_$_^4t@#25v+ss9kc&4i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
